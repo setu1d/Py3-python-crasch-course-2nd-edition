ca9fe7049446b745c2ae36c221953760faeb8d47
setu1d <86005993+setu1d@users.noreply.github.com>
2021-09-13 07:38:44 +0200

    Delete chapter_04 directory

--- chapter_04/first_number.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-"""
-Created on 02.09.2021 12:32:19
-@author: setu1d
-
-"""
-
-for value in range(1,6):
-	print(value)
--- chapter_04/hello_world.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-"""
-Created on 01.09.2021 21:02:32
-@author: setu1d
-
-"""
-
-message = "Hello Python world!"
-	print(message)
